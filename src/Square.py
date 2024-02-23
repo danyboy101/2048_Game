45774f543ab114614124d0b55db5b3224d2cf2d3
Daniel Buruiana <danielburbur@gmail.com>
2024-02-23 00:32:58 -0500

    Added primitive square movement

--- src/Square.py
+++ src/Square.py
@@ -2,14 +2,57 @@ from constants import *
 import random
 
 class Square:
-    def __init__(self, pos):
+    """
+    Grid Diagram:
+        
+        [ 0 , 1 , 2 , 3  ]
+        [ 4 , 5 , 6 , 7  ]
+        [ 8 , 9 , 10, 11 ]
+        [ 12, 13, 14, 15 ]
+
+    """
+    SPEED = 5
+
+    def __init__(self, win, pos):
+        self.win = win
         self.pos = pos
         self.x = 100 + PADDING + (PADDING + SQUARE_SIZE) * (self.pos % 4)
         self.y = 100 + PADDING + (PADDING + SQUARE_SIZE) * (self.pos // 4)
-        self.exp = 1
+        self.exp = 0
+        self.update_exp()
+
+    def update_exp(self):
+        self.exp += 1
         self.value = str(2 ** self.exp)
 
-    def draw_square(self, win):
-        pygame.draw.rect(win, COLOR_2, (self.x, self.y, SQUARE_SIZE, SQUARE_SIZE))
+    def draw_square(self):
+        pygame.draw.rect(self.win, COLOR_2, (self.x, self.y, SQUARE_SIZE, SQUARE_SIZE))
         nb = NB_FONT.render(self.value, 1, BLACK)
-        win.blit(nb, (self.x + SQUARE_SIZE//2 - nb.get_rect()[2]//2, self.y + SQUARE_SIZE//2 - nb.get_rect()[3]//2))
+        self.win.blit(nb, (self.x + SQUARE_SIZE//2 - nb.get_rect()[2]//2, self.y + SQUARE_SIZE//2 - nb.get_rect()[3]//2))
+
+    def move_right(self):
+        self.x += Square.SPEED
+    
+    def move_left(self):
+        self.x -= Square.SPEED
+
+    def move_up(self):
+        self.y -= Square.SPEED
+
+    def move_down(self):
+        self.y += Square.SPEED
+
+    def movement(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.move_up()
+        elif keys[pygame.K_DOWN]:
+            self.move_down()
+        elif keys[pygame.K_RIGHT]:
+            self.move_right()
+        elif keys[pygame.K_LEFT]:
+            self.move_left()
+
+    def getAdj(self):
+        pass
--- src/game_2048.py
+++ src/game_2048.py
@@ -1,5 +1,4 @@
 import pygame
-import random
 
 from Square import *
 from constants import *
@@ -7,28 +6,27 @@ from constants import *
 pygame.init()
 pygame.display.set_caption("2048")
 
-def draw_board(win):
+def draw_board(win, square):
     win.fill(BG_COLOR)
     pygame.draw.rect(win, DARK_GREY, (100, 100, GRID_SIZE, GRID_SIZE))
     for n in range(4):
         for k in range(4):
             pygame.draw.rect(win, LIGHT_GREY, (100 + PADDING + (PADDING + SQUARE_SIZE) * n, 
-                                               100 + PADDING + (PADDING + SQUARE_SIZE) * k, SQUARE_SIZE, SQUARE_SIZE))
+                    100 + PADDING + (PADDING + SQUARE_SIZE) * k, SQUARE_SIZE, SQUARE_SIZE))
+    square.draw_square()
 
 def main():
     run = True
-    
+    s = Square(WIN, 5)
     while run:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
-        draw_board(WIN)
-        s = Square(5)
-        s.draw_square(WIN)
-
+        draw_board(WIN, s)
+        s.movement()
+        
         pygame.display.update()
 
     pygame.quit()
