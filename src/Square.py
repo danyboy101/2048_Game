491e7248e85386991ba63adfe504b3b2fcc703d8
Daniel Buruiana <danielburbur@gmail.com>
2024-06-05 09:46:37 -0400

    Finalized square movement
    Squares now move smoothly and at a convenient speed from one position to the next

--- src/Square.py
+++ src/Square.py
@@ -8,11 +8,34 @@ class Square:
         self.pos = pos
         self.col = self.pos % 4
         self.row = self.pos // 4
-        self.x = 100 + PADDING + (PADDING + SQUARE_SIZE) * self.col
-        self.y = 100 + PADDING + (PADDING + SQUARE_SIZE) * self.row
+        self.x = self.getX()
+        self.y = self.getY()
         self.exp = 2
+
+
+    def getX(self):
+        return 100 + PADDING + (PADDING + SQUARE_SIZE) * self.col
+
+
+    def getY(self):
+        return 100 + PADDING + (PADDING + SQUARE_SIZE) * self.row
+
+
+    def updatePos(self):
+        self.pos = 4 * self.row + self.col
+        self.x = self.getX()
+        self.y = self.getY()
+
 
     def draw_square(self, win):
         pygame.draw.rect(win, COLOR_2, (self.x, self.y, SQUARE_SIZE, SQUARE_SIZE))
         nb = NB_FONT.render(str(self.exp), 1, BLACK)
         win.blit(nb, (self.x + SQUARE_SIZE//2 - nb.get_rect()[2]//2, self.y + SQUARE_SIZE//2 - nb.get_rect()[3]//2))
+
+
+    def moveRight(self):
+        if self.col != 3:
+            self.col += 1
+            while self.x < self.getX():
+                self.x += SPEED
+            self.updatePos()
--- src/game_2048.py
+++ src/game_2048.py
@@ -21,6 +21,7 @@ def main():
     run = True
     clock = pygame.time.Clock()
     s = Square(5)
+    moving = False
 
     while run:
         clock.tick(60)
@@ -30,23 +31,32 @@ def main():
                 run = False
                 break
 
+            if pygame.KEYDOWN:
+
+                keys = pygame.key.get_pressed()
+
+                if keys[pygame.K_UP] and s.row != 0:
+                    s.row -= 1
+                elif keys[pygame.K_DOWN] and s.row != 3:
+                    s.row += 1
+                elif keys[pygame.K_LEFT] and s.col != 0:
+                    s.col -= 1 
+                elif keys[pygame.K_RIGHT] and s.col != 3:
+                    s.col += 1
+
         draw_board(WIN, s)
-
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_UP]:
+        
+        if s.y > s.getY():
             s.y -= SPEED
-            pygame.display.update()
-        elif keys[pygame.K_DOWN]:
+        elif s.y < s.getY():
             s.y += SPEED
-            pygame.display.update()
-        elif keys[pygame.K_LEFT]:
+        elif s.x > s.getX():
             s.x -= SPEED
-            pygame.display.update()
-        elif keys[pygame.K_RIGHT]:
+        elif s.x < s.getX():
             s.x += SPEED
-            pygame.display.update()
-
+        else:
+            s.updatePos()
+        
         pygame.display.update()
 
     pygame.quit()
